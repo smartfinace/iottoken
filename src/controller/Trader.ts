9964c7c7cd57b93249bd00d6d52742bd028c4af6
Roller Davic <sonataruby@gmail.com>
2022-03-11 18:52:30 +0700

    Update Trader.ts

--- src/controller/Trader.ts
+++ src/controller/Trader.ts
@@ -130,7 +130,7 @@ router.get("/finish",async (req: Request, res: Response, next: NextFunction) => 
 	var close = data.close;
 	var close_type = data.type;
 	var telegram = data.telegram;
-	let getOrderInfo = await modules.getOrdersInfoByTelegram(telegram);
+	let getOrderInfo = await modules.getOrdersInfoByTelegram(Number(telegram));
 	var is_access = "Free";
 	if(Number(target) > 1) is_access = "Vip";
 
